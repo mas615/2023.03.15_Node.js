5c62b082ba8cd894c0bbe684d44ce36a987329f9
majun <mas9623@naver.com>
2023-03-17 17:05:04 +0900

    Cookie3

--- Cookie/Cookie.js
+++ Cookie/Cookie.js
@@ -1,13 +1,19 @@
 var http =  require('http');
 var cookie = require('cookie');
 http.createServer(function(request, response){
-    // response.writeHead(200, {
-    //     'Set-Cookie':['yummy=choco','tasty=strawberry']
-    // });
     if(request.headers.cookie !== undefined){
     var cookies = cookie.parse(request.headers.cookie);
     console.log(cookies);
     console.log(cookies.yummy);
     }
+    response.writeHead(200, {
+        'Set-Cookie':[
+            'yummy=choco',
+            'tasty=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24}`,
+            'Securezzz=Securezz; Secure',
+            'HttpOnly=Httponly; HttpOnly'
+        ]
+    });
     response.end("CookieTest!!");
 }).listen(3000);
